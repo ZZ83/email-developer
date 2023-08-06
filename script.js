1f3ae0a253840a3d51788b67c600fbc00da35613
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-08-05 17:36:43 -0700

    Add form validation

--- script.js
+++ script.js
@@ -1,142 +1,98 @@
-const solace         = document.getElementById('solace');
-const shaggys        = document.getElementById('shaggys');
-const dailySentry    = document.getElementById('daily-sentry');
-const overlay        = document.getElementById('overlay');
-const closeModal     = document.getElementById('close-modal');
-const mobileSwitch   = document.getElementById('check-mobile');
-const darkModeSwitch = document.getElementById('check-dark-mode');
-const modalImage     = document.getElementById('modal-image');
-const viewInBrowser  = document.getElementById('view-in-browser');
+const form         = document.getElementById('form');
+const error        = document.getElementById('error');
+const submitButton = document.getElementById('submitButton');
 
-let isSettingsVisible = false;
+const nameInput    = document.getElementById('name');
+const emailInput   = document.getElementById('email');
+const messageInput = document.getElementById('message');
 
-const emailSource = {
-	dailySentry: {
-		url:          "html/newsletter.html",
-		mobileDark:   "images/daily-sentry/mobile-dark.jpg",
-		mobileLight:  "images/daily-sentry/mobile-light.jpg",
-		desktopDark:  "images/daily-sentry/desktop-dark.jpg",
-		desktopLight: "images/daily-sentry/desktop-light.jpg",
-	},
-	shaggys: {
-		url:          "html/promotional.html",
-		mobileDark:   "images/shaggys/mobile-dark.jpg",
-		mobileLight:  "images/shaggys/mobile-light.jpg",
-		desktopDark:  "images/shaggys/desktop-dark.jpg",
-		desktopLight: "images/shaggys/desktop-light.jpg",
-	},
-	solace: {
-		url:          "html/donation-request.html",
-		mobileDark:   "images/solace/mobile-dark.jpg",
-		mobileLight:  "images/solace/mobile-light.jpg",
-		desktopDark:  "images/solace/desktop-dark.jpg",
-		desktopLight: "images/solace/desktop-light.jpg",
-	},
-	paths: {
-		mobileDark:   "",
-		mobileLight:  "",
-		desktopDark:  "",
-		desktopLight: "",
-	},
-	setImagesPath: function(project) {
-		if(project === "daily-sentry") {
-			this.paths.mobileDark   = this.dailySentry.mobileDark;
-			this.paths.mobileLight  = this.dailySentry.mobileLight;
-			this.paths.desktopDark  = this.dailySentry.desktopDark;
-			this.paths.desktopLight = this.dailySentry.desktopLight;
-		} else if (project === "shaggys") {
-			this.paths.mobileDark   = this.shaggys.mobileDark;
-			this.paths.mobileLight  = this.shaggys.mobileLight;
-			this.paths.desktopDark  = this.shaggys.desktopDark;
-			this.paths.desktopLight = this.shaggys.desktopLight;
-		}
-		 else if (project === "solace") {
-			this.paths.mobileDark   = this.solace.mobileDark;
-			this.paths.mobileLight  = this.solace.mobileLight;
-			this.paths.desktopDark  = this.solace.desktopDark;
-			this.paths.desktopLight = this.solace.desktopLight;
-		}
-	}
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
 
-function resetInputs() {
-	mobileSwitch.checked   = false;
-	darkModeSwitch.checked = true;
-}
-
-closeModal.addEventListener('click', function() {
-	overlay.style.display = "none";
-	modalImage.style.maxWidth = "600px";
-	document.body.style.overflow = "initial";
-});
-
-overlay.addEventListener('click', function(event) {
-    if (isSettingsVisible && event.target.classList.contains("overlay")) {
-        overlay.style.display = "none";
-        isSettingsVisible = false;
-		document.body.style.overflow = "initial";
+function validateEmail(input) {
+    var validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (input.value.match(validRegex)) {
+        input.classList.remove("-error");
+        return true;
+    } else {
+		let small = document.createElement('small');
+		small.textContent = 'Please add a valid email';
+		insertAfter(small, emailInput);
+        input.classList.add("-error");
+        return false;
     }
-});
+}
 
-dailySentry.addEventListener('click', function() {
-	document.body.style.overflow = "hidden";
-	isSettingsVisible = true;
-	resetInputs();
-	overlay.style.display = "flex";
-	modalImage.src = emailSource.dailySentry.desktopDark;
-	darkModeSwitch.checked = true;
-	emailSource.setImagesPath("daily-sentry");
-	viewInBrowser.href = emailSource.dailySentry.url;
-});
+function isFormReady() {
+    if ( nameInput.value    !== "" && 
+         emailInput.value   !== "" && 
+         messageInput.value !== "" && 
+         ( validateEmail(emailInput) ) ) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-shaggys.addEventListener('click', function() {
-	document.body.style.overflow = "hidden";
-	isSettingsVisible = true;
-	resetInputs();
-	overlay.style.display = "flex";
-	modalImage.src = emailSource.shaggys.desktopDark;
-	darkModeSwitch.checked = true;
-	emailSource.setImagesPath("shaggys");
-	viewInBrowser.href = emailSource.shaggys.url;
-});
-
-solace.addEventListener('click', function() {
-	document.body.style.overflow = "hidden";
-	isSettingsVisible = true;
-	resetInputs();
-	overlay.style.display = "flex";
-	modalImage.src = emailSource.solace.desktopDark;
-	darkModeSwitch.checked = true;
-	emailSource.setImagesPath("solace");
-	viewInBrowser.href = emailSource.solace.url;
-});
-
-darkModeSwitch.addEventListener('change', function() {
-	if(mobileSwitch.checked && darkModeSwitch.checked) {
-		modalImage.src = emailSource.paths.mobileDark;
-	} else if( mobileSwitch.checked && !darkModeSwitch.checked ) {
-		modalImage.src = emailSource.paths.mobileLight;
-	} else if (!mobileSwitch.checked && !darkModeSwitch.checked) {
-		modalImage.style.maxWidth = "600px";
-		modalImage.src = emailSource.paths.desktopLight;
-	} else if (!mobileSwitch.checked && darkModeSwitch.checked) {
-		modalImage.style.maxWidth = "600px";
-		modalImage.src = emailSource.paths.desktopDark;
+function showActive(element) {
+	if (element.value !== "") {
+        element.classList.add("-active");
+    } else {
+		element.classList.remove("-active");
 	}
+}
+
+nameInput.addEventListener("keyup", () => {
+
+	if (nameInput.classList.contains("-error")) {
+		nameInput.classList.remove("-error");
+		nameInput.nextElementSibling.remove();
+	}
+    showActive(nameInput);
 });
 
-mobileSwitch.addEventListener('change', function() {
-	if( mobileSwitch.checked && !darkModeSwitch.checked ) {
-		modalImage.style.maxWidth = "375px";
-		modalImage.src = emailSource.paths.mobileLight;
-	} else if (!mobileSwitch.checked && darkModeSwitch.checked) {
-		modalImage.style.maxWidth = "600px";
-		modalImage.src = emailSource.paths.desktopDark;
-	} else if (mobileSwitch.checked && darkModeSwitch.checked) {
-		modalImage.style.maxWidth = "375px";
-		modalImage.src = emailSource.paths.mobileDark;
-	} else if (!mobileSwitch.checked && !darkModeSwitch.checked) {
-		modalImage.style.maxWidth = "600px";
-		modalImage.src = emailSource.paths.desktopLight;
+emailInput.addEventListener("keyup", () => {
+	if (emailInput.classList.contains("-error")) {
+		emailInput.classList.remove("-error");
+		emailInput.nextElementSibling.remove();
 	}
+	showActive(emailInput);
+	validateEmail(emailInput);
+});
+
+messageInput.addEventListener("keyup", () => {
+	if (messageInput.classList.contains("-error")) {
+		messageInput.classList.remove("-error");
+		messageInput.nextElementSibling.remove();
+	}
+    showActive(messageInput);
+});
+
+submitButton.addEventListener("click", (event) => {
+    if( !isFormReady() ) {
+        event.preventDefault()
+        if (nameInput.value === "" && !nameInput.classList.contains("-error")) {
+			let small = document.createElement('small');
+			small.textContent = 'This field is required';
+			insertAfter(small, nameInput);
+			nameInput.classList.add("-error");
+        }
+        if (emailInput.value === "" && !emailInput.classList.contains("-error")) {
+			let small = document.createElement('small');
+			small.textContent = 'This field is required';
+			insertAfter(small, emailInput);
+			emailInput.classList.add("-error");
+        }
+        if (messageInput.value === "" && !messageInput.classList.contains("-error")) {
+			let small = document.createElement('small');
+			small.textContent = 'This field is required';
+			insertAfter(small, messageInput);
+			messageInput.classList.add("-error");
+        }
+    } else {
+        form.submit();
+        // form.reset();
+        return false;
+    }
 });
